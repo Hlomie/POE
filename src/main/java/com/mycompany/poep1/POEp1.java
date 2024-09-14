b1c191a67ad306c516e84a7b7d13cd38ffdd473b
RC_Student_lab <RC_Student_lab@10.0.1.40>
2024-09-14 17:11:51 +0000

    Declared username and password as string variable. Set up a scanner to to receive input from the user. Added a print statement to prompt the user to enter their username and password. Included an if statement to check if the entered username and password match predefined values. Provided an appropriate output for the correct or incorrect login details. Added a Regex pattern to validate username format. Added username.contains("_") to ensure that the username contains at least one underscore. Updated Boolean condition for success and error message.

--- src/main/java/com/mycompany/poep1/POEp1.java
+++ src/main/java/com/mycompany/poep1/POEp1.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.poep1;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 /**
  *
  * @author RC_Student_lab
@@ -12,7 +14,7 @@ public class POEp1 {
 
     public static void main(String[] args) {
         
-        //Declarations of variables
+        //Declarations of variables and initializing
         String username;
         String password;
         
@@ -22,6 +24,24 @@ public class POEp1 {
         //Prompt the user to enter their username
         System.out.print("Enter your Username:");
         username = input.nextLine();
+        
+           //Regex: define the pattern for validation
+           Pattern pattern = Pattern.compile( "^(?=.*[a-zA-Z0-9])[a-zA-Z0-9_]+$");
+           Matcher matcher = pattern.matcher(username);
+           
+           //Check if the user name contains an underscore and is no more than 5 characters in length
+           if(username.contains("_") && matcher.matches()){
+               System.out.println("Username is successfully captured");
+           }else{
+               System.out.println("Username is not correctly formatted, please ensure that the username contains an underscore and is no more than 5 characters in length");
+           }
+   
+                   
+           
+          
+          
+          
+          
         
         //Prompt the user to enter their pasword
         System.out.print(" Enter your Password:");
