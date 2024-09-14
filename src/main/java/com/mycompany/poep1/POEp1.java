4aa558096875c9e87adddf74ea4b6dd0c1b73e29
RC_Student_lab <RC_Student_lab@10.0.1.40>
2024-09-14 12:19:59 +0000

    Declared username and password as string variable. Set up a scanner to to receive input from the user. Added a print statement to prompt the user to enter their username and password. Included an if statement to check if the entered username and password match predefined values. Provided an appropriate output for the correct or incorrect login details.

--- src/main/java/com/mycompany/poep1/POEp1.java
+++ src/main/java/com/mycompany/poep1/POEp1.java
@@ -3,7 +3,7 @@
  */
 
 package com.mycompany.poep1;
-
+import java.util.Scanner;
 /**
  *
  * @author RC_Student_lab
@@ -11,6 +11,28 @@ package com.mycompany.poep1;
 public class POEp1 {
 
     public static void main(String[] args) {
+        
+        //Declarations of variables
+        String username;
+        String password;
+        
+        //Create a Scanner to read the outputs
+        Scanner input = new Scanner(System.in);
+        
+        //Prompt the user to enter their username
+        System.out.print("Enter your Username:");
+        username = input.nextLine();
+        
+        //Prompt the user to enter their pasword
+        System.out.print(" Enter your Password:");
+        password = input.nextLine();
+        
+        if(username.equals(" ")&& (password.equals(" "))){
+            System.out.println(" ");
+        }else{
+            System.out.println(" ");
+        }
+        
         
     }
 }
